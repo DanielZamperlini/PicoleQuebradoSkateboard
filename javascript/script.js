2efbd070964312e5287ae48252a09b1b0abb9425
Zamperlini <monkeydluffysan2021@gmail.com>
2023-01-30 17:46:05 -0300

    css configurado

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 if (window.SimpleSlide) {
   new SimpleSlide({
-    slide: "principal", // nome do atributo data-slide="principal"
+    slide: 'principal', // nome do atributo data-slide="principal"
     nav: true, // se deve ou não mostrar a navegação
     auto: true, // se o slide deve passar automaticamente
     time: 4000, // tempo de transição dos slides
